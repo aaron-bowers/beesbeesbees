157c20c0f9732780b5ec5cf6d41ed98578f9f311
Aaron Bowers <bowersaaronjames@gmail.com>
2022-08-08 12:14:25 -0700

    push all local changes to github
    Merge branch 'master' of https://github.com/hackreactor/rfp2204-toy-problems

--- src/Grub.js
+++ src/Grub.js
@@ -1,3 +1,12 @@
+// pushed local file to github
+
 var Grub = function() {
+  this.age = 0;
+  this.color = 'pink';
+  this.food = 'jelly';
 };
 
+Grub.prototype.eat = function(food) {
+  this.ate = true;
+  return 'I am eating';
+};
